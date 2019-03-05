678fc870247941facf6470332961dfdfca5c42f2
roey <roey070500@gmail.com>
2019-03-05 23:39:41 +0200

    get code from the user function added

--- tar1_atm.py
+++ tar1_atm.py
@@ -2,8 +2,19 @@ code = 1234
 amount = 1000
 
 
+def getcode():
+    entered = int(input("please enter the seceret code:"))
+    print(entered)
+    while entered != code:
+        entered = int(input("wrong code,please try again:"))
+
+
 def main():
-    print('hello world')
+    getcode()
+
+
+if __name__ == '__main__':
+    main()
 
 
 
