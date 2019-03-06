0988e620fbba7910ba4104a3b3b911d21831c851
roey <roey070500@gmail.com>
2019-03-06 22:33:42 +0200

    add function sum list from string

--- tar2_sumlist.py
+++ tar2_sumlist.py
@@ -5,11 +5,18 @@ def sumuntilstop():
         sumerize += int(uinput)
         uinput = input("please enter number:")
     print("sum is:",sumerize)
+def sumfromstring():
+    sum = 0
+    uinput = input("enter list of numbers")
+    for i in uinput:
+        if i != ',':
+            sum += int(i)
+    print(sum)
 
 
 def main():
     sumuntilstop()
-
+    sumfromstring()
 
 if __name__ == "__main__":
     main()
