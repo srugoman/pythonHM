5469e70f5ad6f83f48ab4a6307d95b626e041f30
roey <roey070500@gmail.com>
2019-03-06 22:23:58 +0200

    add function sum until stop

--- tar2_sumlist.py
+++ tar2_sumlist.py
@@ -1,6 +1,16 @@
+def sumuntilstop():
+    sumerize = 0
+    uinput = input("please enter number:")
+    while uinput != "stop":
+        sumerize += int(uinput)
+        uinput = input("please enter number:")
+    print("sum is:",sumerize)
 
 
 def main():
-    print("main")
-if __name__== "__main__":
+    sumuntilstop()
+
+
+if __name__ == "__main__":
     main()
+
