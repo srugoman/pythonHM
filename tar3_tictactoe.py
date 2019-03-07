7b4b7fd5897be97c1c686f3270f490577aa74803
roey <roey070500@gmail.com>
2019-03-07 15:54:28 +0200

    add function checkobliques

--- tar3_tictactoe.py
+++ tar3_tictactoe.py
@@ -1,4 +1,5 @@
 def checkrows(game):
+    global player
     for row in game:
         prev = row[0]
         won = True
@@ -6,13 +7,13 @@ def checkrows(game):
             if player != prev:
                 won = False
                 break
-        if won == True:
+        if won:
             return player
     return 0
 
 
 def checkcols(game):
-    first = game [0]
+    first = game[0]
     i = 0
     for player in first:
         prev = player
@@ -21,17 +22,41 @@ def checkcols(game):
             if row[i] != prev:
                 won = False
                 break
-        if won == True:
+        if won:
             return prev
         i += 1
     return 0
 
 
+def checkobliques(game):
+    rowlen = len(game[0]) - 1
+    i = 1
+    prev1 = game[0][0]
+    prev2 = game[0][rowlen]
+    ob1 = True
+    ob2 = True
+    while i <= rowlen:
+        if game[i][i] != prev1:
+            ob1 = False
+        if game[i][rowlen-i] != prev2:
+            ob2 = False
+        if (not ob1) and (not ob2):
+            return 0
+        i += 1
+    if ob1:
+        return prev1
+    if ob2:
+        return prev2
+
+
+
+
+
 def main():
-    game = [[1, 0, 1],
+    game = [[2, 0, 1],
             [2, 1, 0],
-            [2, 1, 1]]
-    print(checkcols(game))
+            [1, 1, 1]]
+    print(checkobliques(game))
 
 
 if __name__ == "__main__":
