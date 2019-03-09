c2447c9fac94a3d4e63112feafc2970a8c6045b1
roey <roey070500@gmail.com>
2019-03-09 14:02:59 +0200

    add function string shrinker

--- tar4_shrinkString.py
+++ tar4_shrinkString.py
@@ -1,8 +1,29 @@
-str = 'abbcccdddd'
+
+def stringShrinker(str):
+    newStr = []
+    index1 = 0
+
+    while index1 < len(str):
+        index2 = index1
+        count = 0
+        while index2 < len(str):
+            if str[index2] == str[index1]:
+                count += 1
+            else:
+                break
+            index2 += 1
+        newStr += [str[index1]]+[count]
+        index1 = index2
+    return newStr
+
+
 
 
 def main():
-    print("main")
+    str = 'abbcccdddd'
+    print("main\n")
+    print(stringShrinker(str))
+
 
 
 if __name__ == "__main__":
