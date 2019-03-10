f5d6c86e13c4fbec2c7614028147bd3a58d3c074
roey <roey070500@gmail.com>
2019-03-10 14:43:41 +0200

    fix function factorial

--- tar7_decorator.py
+++ tar7_decorator.py
@@ -1,14 +1,21 @@
-decorator = []
+#python cannot do recursion over 997
+decorator = [None]*997
 
 
 def factorial(num):
     if num == 1:
         return 1
-    fact = num + factorial(num-1)
+    if decorator[num]:
+        print("decorator used")
+        return decorator[num]
+    fact = num * factorial(num-1)
+    decorator[num] = fact
     return fact
 
+
 def main():
-    print("main")
+    print(factorial(25))
+    print(factorial(30))
 
 
 
